6dc840acfec02f499b45ed233051f8b64fabbd4e
Błażej Bartoszewski <blazejbart@gmail.com>
2024-02-20 23:14:12 +0100

    add enter button

--- src/reducers/ContactForm.js
+++ src/reducers/ContactForm.js
@@ -1,3 +1,67 @@
+// import React, { useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { nanoid } from '@reduxjs/toolkit';
+// import { addContact } from '../reducers/contactsSlice';
+// import styles from './App.module.css';
+
+// const ContactForm = () => {
+//   const [name, setName] = useState('');
+//   const [number, setNumber] = useState('');
+//   const contacts = useSelector(state => state.contacts);
+//   const dispatch = useDispatch();
+
+//   const handleNameChange = event => {
+//     setName(event.target.value);
+//   };
+
+//   const handleNumberChange = event => {
+//     setNumber(event.target.value);
+//   };
+
+//   const handleAddContact = () => {
+//     const isNameExist = contacts.some(
+//       contact =>
+//         contact.name && contact.name.toLowerCase() === name.toLowerCase()
+//     );
+
+//     if (isNameExist) {
+//       alert(`${name} is already in contacts!`);
+//     } else {
+//       dispatch(addContact({ id: nanoid(), name, number }));
+//       setName('');
+//       setNumber('');
+//     }
+//   };
+
+//   return (
+//     <div className={styles.appContainer}>
+//       <p className={styles.name}>Name</p>
+//       <label>
+//         <input
+//           type="text"
+//           name="name"
+//           value={name}
+//           onChange={handleNameChange}
+//         />
+//       </label>
+//       <p className={styles.name}>Phone Number</p>
+//       <label>
+//         <input
+//           type="tel"
+//           name="number"
+//           value={number}
+//           onChange={handleNumberChange}
+//         />
+//       </label>
+//       <button className={styles.addContactBtn} onClick={handleAddContact}>
+//         Add contact
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default ContactForm;
+
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from '@reduxjs/toolkit';
@@ -33,6 +97,12 @@ const ContactForm = () => {
     }
   };
 
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleAddContact();
+    }
+  };
+
   return (
     <div className={styles.appContainer}>
       <p className={styles.name}>Name</p>
@@ -42,6 +112,7 @@ const ContactForm = () => {
           name="name"
           value={name}
           onChange={handleNameChange}
+          onKeyDown={handleKeyPress}
         />
       </label>
       <p className={styles.name}>Phone Number</p>
@@ -51,6 +122,7 @@ const ContactForm = () => {
           name="number"
           value={number}
           onChange={handleNumberChange}
+          onKeyDown={handleKeyPress}
         />
       </label>
       <button className={styles.addContactBtn} onClick={handleAddContact}>
